bc6708f8cd838e7d9030237dac99811c935a929e
neox-lm <opensource@example.com>
2026-04-20 14:54:33 +0800

    chore: configure zpay gateway credentials

--- app/Pay/Epay/Config/Config.php
+++ app/Pay/Epay/Config/Config.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 return [
     'api' => 'https://zpayz.cn',
-    'pid' => '',
-    'key' => '',
+    'pid' => '2026012613465745',
+    'key' => 'zuggb8xhrtLMnSt9WiLUQ5tLV1NHP0U8',
     'sitename' => 'CharityDoing',
     'order_name' => '订单支付',
     'sign_type' => 'MD5'
